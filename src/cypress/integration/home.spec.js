dc5439a8199d27f98863f1ef840aa7ed8eface5f
Andrel Karunia Sitanggang <andrel@tokodistributor.com>
2021-11-10 12:18:43 +0700

    test: Add have created date and functions test

--- src/cypress/integration/home.spec.js
+++ src/cypress/integration/home.spec.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-undef */
-describe('Home Page', () => {
-  it('Should go to Page Detail, click Wishlist and go to Wishlist Page', () => {
+
+import {
+  thousandFormat,
+  dateFormat,
+} from '../../helper';
+
+describe('Adding Wislist Item', () => {
+  it('Should From Home, go to Page Detail, click Star to add to Wishlist and go to Wishlist Page', () => {
     cy.intercept({ method: 'GET', url: 'https://swapi.dev/api/planets?page=1' }, { fixture: 'planets_page_1.json' }).as('getPlanetList');
 
     cy.visit('/');
@@ -33,5 +39,32 @@ describe('Home Page', () => {
     cy.get('a').contains('Wishlist').click();
 
     cy.get('a p').contains('Tatooine');
+  });
+
+  it('Should have created date data on Page Detail', () => {
+    cy.intercept({ method: 'GET', url: 'https://swapi.dev/api/planets/1' }, { fixture: 'detail_tatooine.json' }).as('getDetail');
+
+    cy.visit('/planet/1');
+
+    cy.wait('@getDetail').then(({ response }) => {
+      const { statusCode } = response;
+      expect(statusCode).equal(200);
+    });
+
+    const rowCreated = cy.get('main div').last();
+
+    rowCreated.get('p').last().contains('Tuesday, December 9, 2014');
+  });
+});
+
+describe('Functions', () => {
+  it('Call the thousandFormat function and asert the number', () => {
+    const thousandFormatted = thousandFormat('10000000');
+    expect(thousandFormatted).to.equal('10.000.000');
+  });
+
+  it('Call the thousandFormat function and asert the number', () => {
+    const dateFormatted = dateFormat('2014-12-10T11:35:48.479000Z');
+    expect(dateFormatted).to.equal('Wednesday, December 10, 2014');
   });
 });
